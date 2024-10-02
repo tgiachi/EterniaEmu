d367a4f21d94b00a65dd8a274b6620abd7f1ce1c
Tom <squid@stormwind.it>
2024-10-02 09:40:43 +0200

    feat(PacketFieldAttribute): add Size property to PacketFieldAttribute for field size specification feat(DefaultFieldSize): create DefaultFieldSize class to define common field sizes feat(BuildPacketDefinitionExtension): implement extension method to build packet definitions based on attributes

--- src/EterniaEmu.Network/Attributes/PacketFieldAttribute.cs
+++ src/EterniaEmu.Network/Attributes/PacketFieldAttribute.cs
@@ -3,6 +3,10 @@ namespace EterniaEmu.Network.Attributes;
 [AttributeUsage(AttributeTargets.Field)]
 public class PacketFieldAttribute : Attribute
 {
+    public int Size { get; }
 
-
+    public PacketFieldAttribute(int size)
+    {
+        Size = size;
+    }
 }
---
+++ src/EterniaEmu.Network/Consts/DefaultFieldSize.cs
@@ -0,0 +1,18 @@
+namespace EterniaEmu.Network.Consts;
+
+public static class DefaultFieldSize
+{
+
+    public static int Byte { get; } = 1;
+
+    public static int Dword { get; } = 4;
+
+    public static int Float { get; } = 4;
+
+    public static int Int { get; } = 4;
+
+    public static int Long { get; } = 8;
+
+    public static int Word { get; } = 2;
+
+}
